18f29f76b3a9092f1ff0db75ec0eec74c55e1884
Kelly Street <street.kelly@gmail.com>
2025-03-08 13:24:34 -0800

    Update trajectories_Arianna.R

--- trajectories_Arianna.R
+++ trajectories_Arianna.R
@@ -20,5 +20,34 @@ plot(umap, asp=1, col=colorby(sub$Subcluster), main='All 29 Markers')
 legendby(sub$Subcluster, pos='left')
 
 
+require(mclust)
+mc <- Mclust(pca$x)
+# pick starting cluster (which cluster has highest % of CD3+ CD8+ T-cells?)
+pct <- sapply(sort(unique(mc$classification)), function(clID){
+    sum(mc$classification==clID & sub$Subcluster=="CD3+ CD8+ T-cells") / sum(mc$classification==clID)
+})
+st <- which.max(pct)
+
+
+require(slingshot)
+
+for(k in 5:29){
+    cat('k =',k,'\n')
+    #print(slingLineages(getLineages(pca$x[,1:k], sub$Subcluster, start.clus = "CD3+ CD8+ T-cells")))
+    print(slingLineages(getLineages(pca$x[,1:k], mc$classification, start.clus = st)))
+    cat('\n')
+}
+
+pto <- slingshot(pca$x[,1:20], mc$classification, start.clus = st)
+
+emb <- embedCurves(pto, umap)
+
+plot(umap, asp=1, col=colorby(sub$Subcluster), main='All 29 Markers')
+lines(SlingshotDataSet(emb))
+
+plot(umap, asp=1, col=colorby(assay(pto)[,1], alpha = slingCurveWeights(pto)[,1]), main='All 29 Markers')
+
+plot(umap, asp=1, col= rgb(slingCurveWeights(pto,as.probs=TRUE)[,1], slingCurveWeights(pto,as.probs=TRUE)[,2], slingCurveWeights(pto,as.probs=TRUE)[,3]), main='All 29 Markers')
+
 
 
